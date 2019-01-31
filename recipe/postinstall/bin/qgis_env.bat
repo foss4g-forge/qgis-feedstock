07ad4d5b16fb41be6fe1fcff6a7389dd60a118d4
Larry Shaffer <lshaffer@boundlessgeo.com>
2019-01-31 10:49:11 -0700

    Fix quoting of qgis_env.bat env vars

--- recipe/postinstall/bin/qgis_env.bat
+++ recipe/postinstall/bin/qgis_env.bat
@@ -9,9 +9,9 @@ pushd "%~dp0"
 
   @echo off
   if exist grass_env.bat (
-    path %LIBRARY_PREFIX%\apps\qgis\bin;%GISBASE%\lib;%GISBASE%\bin;%PATH%
+    path "%LIBRARY_PREFIX%\apps\qgis\bin;%GISBASE%\lib;%GISBASE%\bin;%PATH%"
   ) else (
-    path %LIBRARY_PREFIX%\apps\qgis\bin;%PATH%
+    path "%LIBRARY_PREFIX%\apps\qgis\bin;%PATH%"
   )
   set QGIS_PREFIX_PATH=%LIBRARY_PREFIX_POSIX%/apps/qgis
   set GDAL_FILENAME_IS_UTF8=YES
@@ -19,14 +19,14 @@ pushd "%~dp0"
   set VSI_CACHE=TRUE
   set VSI_CACHE_SIZE=1000000
   REM Add qt plugins search path, while honoring any user-set QT_PLUGIN_PATH
-  set QT_PLUGIN_PATH=%LIBRARY_PREFIX%\apps\qgis\qtplugins;%LIBRARY_PREFIX%\plugins;%QT_PLUGIN_PATH%
+  set "QT_PLUGIN_PATH=%LIBRARY_PREFIX%\apps\qgis\qtplugins;%LIBRARY_PREFIX%\plugins;%QT_PLUGIN_PATH%"
 
   REM Extra deployed plugins within %LIBRARY_PREFIX%
   REM Plugins should be installed as expanded plugin archive, via separate conda package
-  set QGIS_PLUGINPATH=%LIBRARY_PREFIX%\apps\qgis-plugins\python;%QGIS_PLUGINPATH%
+  set "QGIS_PLUGINPATH=%LIBRARY_PREFIX%\apps\qgis-plugins\python;%QGIS_PLUGINPATH%"
 
   REM Add qgis's python, while honoring any user-set PYTHONPATH
-  set PYTHONPATH=%CONDA_ROOT%\Lib\site-packages;%LIBRARY_PREFIX%\apps\qgis\python;%PYTHONPATH%
+  set "PYTHONPATH=%CONDA_ROOT%\Lib\site-packages;%LIBRARY_PREFIX%\apps\qgis\python;%PYTHONPATH%"
 
   REM For debugging
   REM set > qgis-env-vars.txt
